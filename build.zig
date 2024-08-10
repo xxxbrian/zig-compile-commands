f856b093276b00fa9141e01b316c2c915aba80aa
Bojin Li <bojinxx@gmail.com>
2024-08-10 23:21:50 +1000

    feat: compile commands generation

---
+++ build.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+const cc = @import("src/root.zig");
+pub const createStep = cc.createStep;
+pub const extractIncludeDirsFromCompileStep = cc.extractIncludeDirsFromCompileStep;
+pub const extractIncludeDirFromInstallFileStep = cc.extractIncludeDirFromInstallFileStep;
+
+pub fn build(b: *std.Build) void {
+    _ = b;
+    @panic("zig-compile-commands is not meant to be built.");
+}
